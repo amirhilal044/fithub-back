16e205f6c2655462721a229a1233bc59f606f7e4
Chris <elhalabichristian@gmail.com>
2024-01-07 20:12:20 +0200

    Refactor authentication controller to use JwtAuthGuard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,22 @@
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { Request as ExpressRequest } from 'express';
 import { AuthService, ResetPasswordDto } from './auth.service';
-import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post('login')
   async login(@Request() req: ExpressRequest): Promise<{
     accessToken: string;
-    user: { id: number; username: string; email: string , userType: string|undefined};
+    user: {
+      id: number;
+      username: string;
+      email: string;
+      userType: string | undefined;
+    };
   }> {
     const { accessToken, user } = await this.authService.login(req.body);
 
